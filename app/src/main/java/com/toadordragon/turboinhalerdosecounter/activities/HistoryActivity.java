e9b6152c6bee5996c7bc6a474d4a30c3b331da92
toadordragon <heymrspaceman@users.noreply.github.com>
2017-12-10 15:11:32 +0000

    Removed some test code

--- app/src/main/java/com/toadordragon/turboinhalerdosecounter/activities/HistoryActivity.java
+++ app/src/main/java/com/toadordragon/turboinhalerdosecounter/activities/HistoryActivity.java
@@ -5,13 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.toadordragon.turboinhalerdosecounter.database.DoseRecorderDBHelper;
 import com.toadordragon.turboinhalerdosecounter.R;
-import com.toadordragon.turboinhalerdosecounter.RemoteFetchJSON;
 import com.toadordragon.turboinhalerdosecounter.fragments.HistoryFragment;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.Iterator;
 
 /**
  * Created by thomas on 07-Mar-17.
@@ -48,43 +42,6 @@ public class HistoryActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.fragmentHistory_container, firstFragment).commit();
         }
-
-        /*doseRecorderDb = DoseRecorderDBHelper.getInstance(this);
-
-        Cursor dayHistoryCursor = doseRecorderDb.getCountsByDay();
-        if (dayHistoryCursor != null) {
-            if (dayHistoryCursor.moveToFirst()) {
-                DayHistoryCursorAdapter myAdapter = new DayHistoryCursorAdapter(this, dayHistoryCursor, 0);
-
-                this.setListAdapter(myAdapter);
-            }
-        }*/
-    }
-
-    private void getHolidayDataFromWebServer_Json()  {
-        new Thread() {
-            public void run() {
-                JSONObject holidayJson = RemoteFetchJSON.getJSON(null); // pass in null as the activity for now
-
-                if (holidayJson != null) {
-                    try {
-                        JSONObject holidaysObject = holidayJson.getJSONObject("holidays");
-                        Iterator iterator1 = holidaysObject.keys();
-                        while(iterator1.hasNext()) {
-                            String key = (String) iterator1.next();
-                            JSONArray singleHolidayArray = holidaysObject.getJSONArray(key);
-                            for (int i =0; i < singleHolidayArray.length(); i++) {
-                                JSONObject singleHolidayJson = singleHolidayArray.getJSONObject(i);
-                                String holidayName = singleHolidayJson.getString("name");
-                                String holidayDate = singleHolidayJson.getString("date");
-                            }
-                        }
-                    } catch (Exception ex) {
-
-                    }
-                }
-            }
-        }.start();
     }
 
     //TODO if we ever put a onRestartr in here then we call changeCursor
